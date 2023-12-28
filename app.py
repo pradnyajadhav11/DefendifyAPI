4a2e3ab71ba98bfd2be47336b676183a98d7f0ae
Pradnya Jadhav <95834572+pradnyajadhav11@users.noreply.github.com>
2023-12-28 19:12:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def malwaredetection():
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=500)
